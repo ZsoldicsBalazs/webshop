e03ef0c910eb81b74074ca4cfbaed88bcfa7a212
ZsoldicsBalazs <119108270+ZsoldicsBalazs@users.noreply.github.com>
2024-12-03 13:01:44 +0200

    create db connection, test controller, product model, jpa

--- src/main/java/com/balazs/spring_webshop_api/SpringWebshopApiApplication.java
+++ src/main/java/com/balazs/spring_webshop_api/SpringWebshopApiApplication.java
@@ -8,6 +8,7 @@ public class SpringWebshopApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringWebshopApiApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/balazs/spring_webshop_api/model/Product.java
@@ -0,0 +1,31 @@
+package com.balazs.spring_webshop_api.model;
+
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private long store_id;
+    private String category;
+    private Date releaseDate;
+    private boolean productAvailable;
+    private int stockQuantity;
+}
