3d6a15945e6351001a3bf59eef6133662699893d
ZsoldicsBalazs <119108270+ZsoldicsBalazs@users.noreply.github.com>
2024-12-03 12:31:38 +0200

    first commit

---
+++ src/main/java/com/balazs/spring_webshop_api/SpringWebshopApiApplication.java
@@ -0,0 +1,13 @@
+package com.balazs.spring_webshop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebshopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebshopApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balazs/spring_webshop_api/SpringWebshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balazs.spring_webshop_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebshopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
